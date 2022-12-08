4c8b0fab28f4804803597275af3ed388e74c3e69
RickSekuloski <66051893+RickSekuloski@users.noreply.github.com>
2022-12-08 22:16:13 +1100

    Create lecture6.py

---
+++ part1/lecture6.py
@@ -0,0 +1,16 @@
+# Sixth Lecture from part1 ---- Integer Data Type ----
+# Important! if you want to run this lecture on replit.com
+# you need to copy and paste the content into the main.py
+# file and press the run button.
+
+# Integer Data type
+# whole numbers
+# basic calcuations using +, -, *, /
+a = 6
+b = 3
+print(a + b)
+print(a * b)
+print(a / b)
+print(a - b)
+print(type(a))
+print(type(b))
