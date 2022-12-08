161f3f463de782c682cc009696a37543bdbc1c12
RickSekuloski <66051893+RickSekuloski@users.noreply.github.com>
2022-12-08 22:16:34 +1100

    Create lecture7.py

---
+++ part1/lecture7.py
@@ -0,0 +1,13 @@
+# Seventh Lecture from part1 ---- Float - Floating Point Number ----
+# Important! if you want to run this lecture on replit.com
+# you need to copy and paste the content into the main.py
+# file and press the run button.
+
+# Numbers with decimal part like 3.5, -6.3
+m = 3.2
+n = 1.7
+
+print(m * n)
+
+#type
+print(type(n))
