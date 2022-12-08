c35a9255a837850fc9fe981e03fd7bd47b670ba1
RickSekuloski <66051893+RickSekuloski@users.noreply.github.com>
2022-12-08 22:15:56 +1100

    Create lecture5.py

---
+++ part1/lecture5.py
@@ -0,0 +1,29 @@
+# Fifth Lecture from part1
+# Important! if you want to run this lecture on replit.com
+# you need to copy and paste the content into the main.py
+# file and press the run button.
+
+print('Hi')
+print('Hi There')
+my_name = 'Rick'
+print(my_name)
+my_age = 10
+print(my_age)
+my_input = input('What is your last name? ')
+print(my_input)
+first, second = 1, 2
+print(first)
+print(second)
+
+# Single line comment
+
+'''
+Multi-line
+comment
+'''
+
+#Int
+#float
+#Complex
+#Bool
+#Str
