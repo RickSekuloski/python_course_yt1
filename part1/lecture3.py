b80822af7cce4f91e00f03a36a758715cc3786ef
RickSekuloski <66051893+RickSekuloski@users.noreply.github.com>
2022-12-08 22:15:11 +1100

    Create lecture3.py

---
+++ part1/lecture3.py
@@ -0,0 +1,6 @@
+# Third Lecture from part1 (first two were theory)
+# Named FirstPythonProject in replit.com
+# Important! if you want to run this lecture on replit.com
+# you need to copy and paste the content into the main.py
+# file and press the run button.
+print('Hi')
